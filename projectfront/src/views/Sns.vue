323a35efd35f782af07aab1d04ee6ac097f26671
seunggue <seungue1687@gmail.com>
2020-02-20 18:09:50 +0900

    2.20일 18시09분

---
+++ projectfront/src/views/Sns.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="SNS">
+    <section class="blog-post-area">
+      <div>
+        <img src="img/snsmain.png" style="width:100%;">
+    </div>
+      <div class ="container">
+        <div class = "row">
+            <Com_Sns_Right/>
+            <Com_Sns_Left/>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Com_Sns_Left from '@/components/Com_Sns_Left.vue'
+import Com_Sns_Right from '@/components/Com_Sns_Right.vue'
+
+export default {
+  name: 'SNS',
+  components: {
+    Com_Sns_Left,
+    Com_Sns_Right,
+  }
+}
+</script>
